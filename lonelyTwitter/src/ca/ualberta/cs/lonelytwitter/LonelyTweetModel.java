a1d19d1179862493ad27ca254eccdf5a121e7b17
peijen <peijen@ualberta.ca>
2015-01-29 16:45:36 -0700

    fixed equal and add function in lonelytwittermodel

--- lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -38,6 +38,7 @@ public abstract class LonelyTweetModel {
 		}
 		
 		LonelyTweetModel otherTweet = (LonelyTweetModel) other;
+		
 		return timestamp.equals(otherTweet.timestamp) && text.equals(otherTweet.text);
 	}
 }
