47f69c90bb41d0280e797b1bd5a97ed7f2754e18
peijen <peijen@ualberta.ca>
2015-02-12 15:54:31 -0700

    lab 6 intentTesting

--- lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
@@ -34,6 +34,23 @@ public class LonelyTwitterActivityUITest extends
 		textInput = ((EditText) activity.findViewById(ca.ualberta.cs.lonelytwitter.R.id.body));
 	}
 	
+	public void testSetText()
+	{
+		
+		instrumentation.runOnMainSync(new Runnable(){
+			@Override
+			public void run(){
+				String text = "tweet";
+				textInput.setText(text);
+			}
+		});
+		
+		instrumentation.waitForIdleSync();
+		
+		assertEquals("correct text?", "tweet", textInput.getText().toString());
+		
+	}
+	
 	/*
 	 * fills in the input text field and clicks the 'save'
 	 * button for the activity under test
@@ -42,5 +59,8 @@ public class LonelyTwitterActivityUITest extends
 		assertNotNull(activity.findViewById(ca.ualberta.cs.lonelytwitter.R.id.save));
 		textInput.setText(text);
 		((Button) activity.findViewById(ca.ualberta.cs.lonelytwitter.R.id.save)).performClick();
+		assertEquals("added a tweet?",text,textInput.getText().toString());
+		
+		
 	}
 }
